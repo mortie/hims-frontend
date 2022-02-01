c96cfc07020625f23d7825526c856689c5c4a9c8
sroyhispindia <subham.roy@hispindia.org>
2022-02-01 21:46:51 +0530

    Amount, retunedamount,discount  functionality added in Billing module

--- src/services/data/index.js
+++ src/services/data/index.js
@@ -1,46 +1,32 @@
-import {
-    Location
-} from './locationData'
+import { Location } from "./locationData";
 
-import {
-    Visit
-} from './visitData'
+import { Visit } from "./visitData";
 
-import {
-    Concept
-} from './Concept'
+import { Concept } from "./Concept";
 
-import {
-    Patient
-} from './Patient'
+import { Patient } from "./Patient";
 
-import {
-    Encounter
-} from './Encounter'
+import { Encounter } from "./Encounter";
 
-import {
-    PatientHistory
-} from './PatientHistory'
+import { PatientHistory } from "./PatientHistory";
 
-import {
-    OrderGeneration
-} from './OrderGeneration'
+import { OrderGeneration } from "./OrderGeneration";
 
-import {
- PatientSearchData 
-} from './PatientSearchData'
+import { PatientSearchData } from "./PatientSearchData";
 
-import {
-    TestOrderDetails
-} from './TestOrderDetails'
+import { TestOrderDetails } from "./TestOrderDetails";
+
+import { SaveBillingPostData } from "./SaveBillingPostData";
+
 export {
-    Location,
-    Visit,
-    Concept,
-    Patient,
-    Encounter,
-    PatientHistory,
-    OrderGeneration,
-    PatientSearchData,
-    TestOrderDetails 
+  Location,
+  Visit,
+  Concept,
+  Patient,
+  Encounter,
+  PatientHistory,
+  OrderGeneration,
+  PatientSearchData,
+  TestOrderDetails,
+  SaveBillingPostData,
 };
