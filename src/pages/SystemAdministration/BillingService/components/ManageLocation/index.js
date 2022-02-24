cf0b6c009591edbfd4faee6e65388b5708be54c5
Martin Dørum <martid0311@gmail.com>
2022-02-24 22:24:34 +0100

    load and store mappings from and to the back-end

--- src/pages/SystemAdministration/BillingService/components/ManageLocation/index.js
+++ src/pages/SystemAdministration/BillingService/components/ManageLocation/index.js
@@ -15,17 +15,20 @@ import {
   ListSubheader} from '@material-ui/core/';
 import styles from "../../styles";
 import { Location, Category } from '../../../../../services/data';
-import { getAPI } from '../../../../../services';
+import { getAPI, postAPI } from '../../../../../services';
 
 const useStyles = makeStyles(styles);
 function ManageLocation() {
-  var [category, setCategory] = useState({});
-  var [categories, setCategories] = useState([]);
-  var [categoriesLoaded, setCategoriesLoaded] = useState(false);
-  var [locationsLoaded, setLocationsLoaded] = useState(false);
-  var [unassignedLocations, setUnassignedLocations] = useState(null);
+  let [category, setCategory] = useState({});
+  let [categories, setCategories] = useState([]);
+  let [dataLoaded, setDataLoaded] = useState(false);
+  let [unassignedLocations, setUnassignedLocations] = useState(null);
   let [currentCategoryLocations, setCurrentCategoryLocations] = useState([]);
   let [assignedLocations, setAssignedLocations] = useState({});
+  let [hasChanged, setHasChanged] = useState(false);
+  let [currentlySaving, setCurrentlySaving] = useState(false);
+
+  let [movements, setMovements] = useState({});
 
   function updateAssignedLocations() {
     let newLocations = {...assignedLocations};
@@ -43,15 +46,63 @@ function ManageLocation() {
   }
 
   useEffect(() => {
-    Location.getAll({}).then(locations => {
-      sortAndSetLocations(setUnassignedLocations, locations);
-      setLocationsLoaded(true);
-    });
+    let locations, categories, mappings;
 
-    Category.getAll({}).then(categories => {
-      setCategory({...categories[0]});
+    Promise.all([
+      Location.getAll({}).then(locs => locations = locs),
+      Category.getAll({}).then(cats => categories = cats),
+      getAPI("/categoryLocation/mapping")
+        .then(res => mappings = res.data),
+    ]).then(() => {
+      let locationsByUUID = {};
+      for (let location of locations) {
+        locationsByUUID[location.uuid] = location;
+      }
+
+      let categoriesByUUID = {};
+      for (let category of categories) {
+        categoriesByUUID[category.uuid] = category;
+      }
+
+      let locationsByCategoryUUID = {};
+      for (let categoryUUID in mappings) {
+        locationsByCategoryUUID[categoryUUID] = [];
+        for (let mapping of mappings[categoryUUID]) {
+          locationsByCategoryUUID[categoryUUID].push(locations[mapping.locationUuid]);
+        }
+      }
+
+      let mappingByLocationUUID = {};
+      for (let categoryUUID in mappings) {
+        for (let mapping of mappings[categoryUUID]) {
+          let locationUUID = mapping.locationUuid;
+          mappingByLocationUUID[locationUUID] = categoryUUID;
+        }
+      }
+
+      let unassignedLocations = [];
+      let assignedLocations = {};
+      assignedLocations[categories[0].uuid] = [];
+      for (let location of locations) {
+        if (mappingByLocationUUID[location.uuid]) {
+          let categoryUUID = mappingByLocationUUID[location.uuid];
+          if (assignedLocations[categoryUUID] == null) {
+            assignedLocations[categoryUUID] = [];
+          }
+          assignedLocations[categoryUUID].push(location);
+        } else {
+          unassignedLocations.push(location);
+        }
+      }
+
+      sortAndSetLocations(setCurrentCategoryLocations, assignedLocations[categories[0].uuid]);
+      sortAndSetLocations(setUnassignedLocations, unassignedLocations);
+      setAssignedLocations(assignedLocations);
+
+      setCategory(categories[0]);
       setCategories(categories);
-      setCategoriesLoaded(true);
+
+      setDataLoaded(true);
     });
   }, []);
 
@@ -62,6 +113,20 @@ function ManageLocation() {
     sortAndSetLocations(setUnassignedLocations, unassignedLocations);
     currentCategoryLocations.push(location);
     sortAndSetLocations(setCurrentCategoryLocations, currentCategoryLocations);
+
+    let movement = movements[location.uuid] || {from: null};
+    movement.to = category.uuid;
+    if (movement.to == movement.from) {
+      movement = null;
+    }
+
+    if (movement.to == movement.from) {
+      delete movements[location.uuid];
+    } else {
+      movements[location.uuid] = movement;
+    }
+
+    setHasChanged(Object.keys(movements).length > 0);
   }
 
   function removeLocation(location) {
@@ -71,6 +136,16 @@ function ManageLocation() {
     sortAndSetLocations(setCurrentCategoryLocations, currentCategoryLocations);
     unassignedLocations.push(location);
     sortAndSetLocations(setUnassignedLocations, unassignedLocations);
+
+    let movement = movements[location.uuid] || {from: category.uuid};
+    movement.to = null;
+    if (movement.to == movement.from) {
+      delete movements[location.uuid];
+    } else {
+      movements[location.uuid] = movement;
+    }
+
+    setHasChanged(Object.keys(movements).length > 0);
   }
 
   function changeCategory(newCategory) {
@@ -83,12 +158,63 @@ function ManageLocation() {
     }
   }
 
+  async function save() {
+    setCurrentlySaving(true);
+
+    let deletes = {};
+    let inserts = {};
+    for (let locationUUID in movements) {
+      let movement = movements[locationUUID];
+      if (movement.from != null) {
+        if (deletes[movement.from] == null) {
+          deletes[movement.from] = [locationUUID];
+        } else {
+          deletes[movement.from].push(locationUUID);
+        }
+      }
+
+      if (movement.to != null) {
+        if (inserts[movement.to] == null) {
+          inserts[movement.to] = [locationUUID];
+        } else {
+          inserts[movement.to].push(locationUUID);
+        }
+      }
+    }
+
+    for (let categoryUUID in deletes) {
+      console.log("Deletes for", categoryUUID,
+        await postAPI("/categoryLocation/mapping", {
+          priceCategoryConUuid: categoryUUID,
+          locationUuids: deletes[categoryUUID],
+          deleted: true,
+        })
+      );
+    }
+
+    for (let categoryUUID in inserts) {
+      await postAPI("/categoryLocation/mapping", {
+        priceCategoryConUuid: categoryUUID,
+        locationUuids: inserts[categoryUUID],
+        deleted: false,
+      })
+    }
+
+    setMovements({});
+    setHasChanged(false);
+    setCurrentlySaving(false);
+  }
+
+  let blockInput = !dataLoaded || currentlySaving;
+
   function Locations() {
     return (
       <Grid container spacing={1}>
         <Grid item style={{flexGrow: 1, width: "50%"}}>
           <Paper>
-            <List subheader={<ListSubheader>Unassigned locations</ListSubheader>}>
+            <List subheader={
+              <ListSubheader>Unassigned locations</ListSubheader>}
+            >
               {unassignedLocations.length == 0 && <ListItem>No items</ListItem>}
               {unassignedLocations.map((location, idx) => (
                 <ListItem
@@ -97,7 +223,7 @@ function ManageLocation() {
                   divider={idx != unassignedLocations.length - 1}
                 >
                   {location.display}
-                  <Button onClick={() => addLocation(location)}>
+                  <Button onClick={() => addLocation(location)} disabled={currentlySaving}>
                     <Icon className="fa fa-chevron-right" />
                   </Button>
                 </ListItem>
@@ -107,7 +233,11 @@ function ManageLocation() {
         </Grid>
         <Grid item style={{flexGrow: 1, width: "50%"}}>
           <Paper>
-            <List subheader={<ListSubheader>Locations assigned to {category.display}</ListSubheader>}>
+            <List subheader={
+              <ListSubheader style={{textAlign: "right"}}>
+                Locations assigned to {category.display}
+              </ListSubheader>}
+            >
               {currentCategoryLocations.length == 0 && <ListItem>No items</ListItem>}
               {currentCategoryLocations.map((location, idx) => (
                 <ListItem
@@ -115,7 +245,7 @@ function ManageLocation() {
                   style={{justifyContent: "space-between"}}
                   divider={idx != currentCategoryLocations.length - 1}
                 >
-                  <Button onClick={() => removeLocation(location)}>
+                  <Button onClick={() => removeLocation(location)} disabled={currentlySaving}>
                     <Icon className="fa fa-chevron-left" />
                   </Button>
                   {location.display}
@@ -140,28 +270,65 @@ function ManageLocation() {
       }
       className={classes.root}
     >
+      <Grid container spacing={1}>
+        <Grid item style={{flexGrow: 1}}>
+          <FormControl
+            variant="outlined"
+            fullWidth
+            margin="dense"
+            className={classes.field}
+            disabled={blockInput}
+          >
+            <InputLabel id="category">Category</InputLabel>
+            <Select
+              labelId="categoryLabel"
+              label="Category"
+              value={category.uuid || ""}
+              onChange={(e) => changeCategory(categories[e.target.value])}
+            >
+              {categories.map(cat =>
+              <MenuItem key={cat.uuid} value={cat.uuid}>{cat.display}</MenuItem>)}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl
+            variant="outlined"
+            fullWidth
+            margin="dense"
+            className={classes.field}
+          >
+            <Button
+              disabled={!hasChanged || currentlySaving}
+              onClick={save} variant="outlined"
+            >
+              Save
+            </Button>
+          </FormControl>
+        </Grid>
+      </Grid>
+
+      <div style={{height: "5px"}}>
+        {currentlySaving && <LinearProgress />}
+      </div>
+
+      {dataLoaded
+        ? Locations()
+        : <LinearProgress />}
+
       <FormControl
         variant="outlined"
         fullWidth
         margin="dense"
         className={classes.field}
       >
-        <InputLabel id="category">Category</InputLabel>
-        <Select
-          labelId="categoryLabel"
-          label="Category"
-          value={category.uuid || ""}
-          onChange={(e) => changeCategory(categories[e.target.value])}
+        <Button
+          disabled={!hasChanged || currentlySaving}
+          onClick={save} variant="outlined"
         >
-          {categories.map(cat =>
-            <MenuItem key={cat.uuid} value={cat.uuid}>{cat.display}</MenuItem>)}
-        </Select>
+          Save
+        </Button>
       </FormControl>
-
-      {locationsLoaded && categoriesLoaded
-        ? Locations()
-        : <LinearProgress />
-      }
     </List>
   )
 }
